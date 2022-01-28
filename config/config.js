b6f177ff213fcc70a26966c25b79c92ae84e968b
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-01-27 23:47:07 -0500

    adding more settings to docker

---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  MONGO_IP: process.env.MONGO_IP || 'mongo',
+};
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose
+  .connect('mongodb://costa:mypassword@mongo:27017/?authSource=admin')
+  .then(() => {
+    console.log('Successfully connected to DB');
+  })
+  .catch((e) => console.log(e));
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi There!!!</h2>');
