b48fcc854a2118eacd86f71158cfa71c34c0b48a
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-02-11 01:22:44 -0500

    created auth middleware for authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,9 @@ exports.signUp = async (req, res) => {
       username,
       password: hashpassword,
     });
+
+    req.session.user = newUser;
+
     res.status(201).json({
       status: 'success',
       data: {
@@ -40,6 +43,8 @@ exports.login = async (req, res) => {
     const isCorrect = await bcrypt.compare(password, user.password);
 
     if (isCorrect) {
+      req.session.user = user;
+
       res.status(200).json({
         status: 'success',
       });
